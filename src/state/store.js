f4cd640a9fe732e3daf885f2f6bab6f51f5e0571
kaushik <kaushik@gmail.com>
2021-09-24 10:02:00 +0530

    Added Dispathcer and thunk

--- src/state/store.js
+++ src/state/store.js
@@ -1,7 +1,9 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
 import reducers from './reducers';
+import thunk from 'redux-thunk'
 
 export const store = createStore(
   reducers,
-  {} // default state
+  {}, // default state
+  applyMiddleware(thunk)
 );
